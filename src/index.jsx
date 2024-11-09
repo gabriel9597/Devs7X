9b0c28376f8a8d82af372ddbde25b387656cc426
gabriel9597 <gabrielsamurai959@gmail.com>
2024-11-09 13:50:07 -0300

    retirei o jquery

--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,6 @@ import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
 
-import "../node_modules/jquery/dist/jquery.min.js";
-import "../node_modules/lightslider/dist/css/lightslider.css";
-import "../node_modules/lightslider/dist/js/lightslider.js"
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
