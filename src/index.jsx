738d215859a14717ec77b016753f017d1d7013ef
gabriel9597 <gabrielsamurai959@gmail.com>
2024-11-10 00:07:54 -0300

    atualization 7

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './Global.css';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
