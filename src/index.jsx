dd31fedd8341c63c28b35d5979c8aefc618179ec
gabriel9597 <gabrielsamurai959@gmail.com>
2024-11-11 01:21:25 -0300

    at 20

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Global.css';
-import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
