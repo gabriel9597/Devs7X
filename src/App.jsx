b35759ab85c8b89ffb480282848309ca6f70b365
gabriel9597 <gabrielsamurai959@gmail.com>
2024-11-25 21:13:20 -0300

    at 43

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Inicio from './Components/Inicio/inicio';
 import Servicos from './Components/services/services';
 import Contato from './Components/contact/contact';
 import ScrollToTop from './Components/scrollToTop';
 
+import ReactGA from 'react-ga4'
 function App() {
+  useEffect(()=> {
+    ReactGA.initialize("G-YGHPW4N0FF");
+
+    ReactGA.send({hitType: "pageview", page: window.location.pathname, title: "App.jsx"})
+  }, [])
   return <Router>
       <ScrollToTop />
       <Routes>
